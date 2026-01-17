723966f886f6ca1604b46fe0c4567cd3145752a5
Mirmeks-dev <mirmeks.dev@gmail.com>
2026-01-17 11:12:17 +0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,10 +1,10 @@
 --Game checker
-if game.PlaceId == 3260590327 then
+if game.PlaceId ~= 3260590327 then
   return
 end
 
 local GameType = Workspace.Type
-if GameType.Value == "Lobby" then
+if GameType.Value ~= "Game" then
   return
 end  
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Mirmeks-dev/TDS-Autoupgrade/main/TDSUpgrade.lua"))()
